346ccb21d41ccc9ec3912e4f215d59ce39b709a8
TheRimura <158588649+TheRimura@users.noreply.github.com>
2025-05-14 14:53:06 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import NavBar from "./component/NavBar";
+import OtroComponente from "./component/Bienvenida";
+
+function App() {
+  return (
+    <div>
+    
+      <NavBar />
+      <OtroComponente />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Bienvenida.js
@@ -0,0 +1,8 @@
+export default function Bienvenida() {
+    return (
+      <div className="p-6 text-center text-lg text-gray-700">
+        Hola, Bienvenido a mi NavBar
+      </div>
+    );
+  }
+  
